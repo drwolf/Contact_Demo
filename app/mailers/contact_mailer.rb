46af6ead9856d55c4a696769f41af3526f6431e9
Robert Wolf <derrobertwolf@gmx.de>
2015-04-25 17:41:47 +0200

    first commit

---
+++ app/mailers/contact_mailer.rb
@@ -0,0 +1,31 @@
+class ContactMailer < ActionMailer::Base
+  #layout 'contact'
+  #default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.confirmation.subject
+  #
+  def confirmation(email)
+    attachments['rails.png'] = File.read("#{Rails.root}/app/assets/images/rails.png")
+    
+    mail(to: email, from: "info@railsbuch.de", subject:
+    "Ihre Kontaktnachricht erhalten") do |format|
+      format.html { render layout: 'contact' }
+      format.text
+    end
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.contact_mailer.inquiry.subject
+  #
+  def inquiry(contact_message)
+    @contact_message = contact_message
+      mail to: "newsletterstar@gmail.com",
+            from: contact_message.email,
+            subject: "Neue Kontaktanfrage"
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContactDemo::Application.config.secret_token = '103b3a56221f7c355613b0f23e7bcfa7fc7e5a98c56ce99cf8984729e168fb12ae2022c92cc3f3cfba5056fe00b0f63d88a673696999390583ed190e22b424e6'
