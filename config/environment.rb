3e100a3c120c2cb35ebc3cc390f3c518a940876a
Robert Wolf <derrobertwolf@gmx.de>
2015-05-03 12:24:46 +0200

    table with hover over Anzeigen Bearbeiten Löschen

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 ContactDemo::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'drwolf'],
+  :password       => 'l1u2p3u4'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
